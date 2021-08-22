cc51ae844e2610714d70b99fa4cc1a8021e9cca1
Alexandre Oliveira <asoaresdeoliveira977@gmail.com>
2021-08-21 21:45:23 -0300

    usando massa de dados

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -2,8 +2,9 @@
 var faker = require('faker');
 
 describe('Funcionalidade Pré Cadastro', () =>{
+    
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
     });
 
     it('Deve completar o pré cadastro com sucesso', () =>{
