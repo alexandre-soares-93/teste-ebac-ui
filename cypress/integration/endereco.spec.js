54b45019eb3c1621b95fe128e3e3c579986c20c7
Alexandre Oliveira <asoaresdeoliveira977@gmail.com>
2021-08-23 21:33:40 -0300

    Criando Page Objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -8,8 +9,8 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         })
         
     });
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-        //cadastro de endereço
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Kenenisa', 'Bekele', 'NN Runing Team', 'Brasi', 'Rua Pio X', '100', 'Osasco', 'São Paulo', '06234530','11-988204567', 'KB@nn.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
